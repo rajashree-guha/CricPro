a8240a53355d8778df5c540e3f151d7343946dd7
rajashree-guha <rajashree.625.guha@gmail.com>
2023-10-26 13:53:27 +0530

    Cric Pro

---
+++ app.js
@@ -0,0 +1,106 @@
+// declaring variables to select the names 
+var sehwagName = document.querySelector(".Sehwag-name")
+var sachinName = document.querySelector(".Sachin-name")
+var gambhirName = document.querySelector(".Gambhir-name")
+var kohliName = document.querySelector(".Kohli-name")
+var dhoniName = document.querySelector(".Dhoni-name")
+var yuvrajName = document.querySelector(".Yuvraj-name")
+var rainaName = document.querySelector("#Raina-name")
+var harbhajanName = document.querySelector("#Harbhajan-name")
+var zaheerName = document.querySelector("#Zaheer-name")
+var munafName = document.querySelector("#Munaf-name")
+var sreesanthName = document.querySelector("#Sreesanth-name")
+
+// declaring variables to select the images 
+var sehwagImg = document.getElementById("sehwag")
+var sachinImg = document.getElementById("sachin")
+var gambhirImg = document.getElementById("gambhir")
+var kohliImg = document.getElementById("kohli")
+var dhoniImg = document.getElementById("dhoni")
+var yuvrajImg = document.getElementById("yuvraj")
+var rainaImg = document.getElementById("raina")
+var harbhajanImg = document.getElementById("harbhajan")
+var zaheerImg = document.getElementById("zaheer")
+var munafImg = document.getElementById("munaf")
+var sreesanthImg = document.getElementById("sreesanth")
+
+// displaying images when clicked on name 
+
+// sehwag 
+sehwagName.addEventListener( "click",()=>{
+    sehwagImg.style.display="block"
+    sehwagImg.onmouseleave=()=>{
+    sehwagImg.style.display="none";
+    }
+})
+// sachin 
+sachinName.addEventListener( "click",()=>{
+    sachinImg.style.display="block"
+    sachinImg.onmouseleave=()=>{
+    sachinImg.style.display="none";
+    }
+})
+// gambhir 
+gambhirName.addEventListener( "click",()=>{
+    gambhirImg.style.display="block"
+    gambhirImg.onmouseleave=()=>{
+    gambhirImg.style.display="none";
+    }
+})
+// kohli 
+kohliName.addEventListener( "click",()=>{
+    kohliImg.style.display="block"
+    kohliImg.onmouseleave=()=>{
+    kohliImg.style.display="none";
+    }
+})
+// dhoni 
+dhoniName.addEventListener( "click",()=>{
+    dhoniImg.style.display="block"
+    dhoniImg.onmouseleave=()=>{
+    dhoniImg.style.display="none";
+    }
+})
+// yuvraj 
+yuvrajName.addEventListener( "click",()=>{
+    yuvrajImg.style.display="block"
+    yuvrajImg.onmouseleave=()=>{
+    yuvrajImg.style.display="none";
+    }
+})
+// raina 
+rainaName.addEventListener( "click",()=>{
+    rainaImg.style.display="block"
+    rainaImg.onmouseleave=()=>{
+    rainaImg.style.display="none";
+    }
+})
+// harbhajan
+harbhajanName.addEventListener( "click",()=>{
+    harbhajanImg.style.display="block"
+    harbhajanImg.onmouseleave=()=>{
+    harbhajanImg.style.display="none";
+    }
+})
+// zaheer 
+zaheerName.addEventListener( "click",()=>{
+    zaheerImg.style.display="block"
+    zaheerImg.onmouseleave=()=>{
+    zaheerImg.style.display="none";
+    }
+})
+// munaf 
+munafName.addEventListener( "click",()=>{
+    munafImg.style.display="block"
+    munafImg.onmouseleave=()=>{
+    munafImg.style.display="none";
+    }
+})
+// sreesanth 
+sreesanthName.addEventListener( "click",()=>{
+    sreesanthImg.style.display="block"
+    sreesanthImg.onmouseleave=()=>{
+    sreesanthImg.style.display="none";
+    }
+})
+
